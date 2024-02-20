f63ededa1b7b40997713826f240383b637f93b29
kshitijsharma0506 <kshitijsharmakota@gmail.com>
2024-02-21 03:48:22 +0530

    Investment App completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const validInput = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header/>
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      {validInput?<Results userInput={userInput}/>:<p className="center">Please Enter a duration greater than 0</p>}
+    </>
   )
 }
 
